a9e1cf7e9c67deb8acac6d68b458ccc0fa483abb
Ivan Malorodov <ivan.malorodov@sap.com>
2023-12-12 08:22:15 +0000

    customer app / bullet chart

--- srv/travel-service.js
+++ srv/travel-service.js
@@ -74,14 +74,19 @@ init() {
   /**
    * Update the Travel's TotalPrice when a Supplement's Price is modified.
    */
-  this.after ('PATCH', 'BookingSupplement', async (_,req) => { if ('Price' in req.data) {
-    // We need to fetch the Travel's UUID for the given Supplement target
-    const { travel } = await SELECT.one `to_Travel_TravelUUID as travel` .from (Booking.drafts)
-      .where `BookingUUID = ${ SELECT.one `to_Booking_BookingUUID` .from (BookingSupplement.drafts).where({BookSupplUUID:req.data.BookSupplUUID}) }`
+  this.after ('PATCH', 'BookingSupplement', async (_,req) => {
+    if ('Price' in req.data) {
+      // We need to fetch the Travel's UUID for the given Supplement target
+      const { booking } = await SELECT.one`to_Booking_BookingUUID as booking`
+        .from(BookingSupplement.drafts).where({ BookSupplUUID: req.data.BookSupplUUID })
+      const { travel } = await SELECT.one`to_Travel_TravelUUID as travel`.from(Booking.drafts)
+        .where`BookingUUID = ${booking} `
       // .where `BookingUUID = ${ SELECT.one `to_Booking_BookingUUID` .from (req._target) }`
       //> REVISIT: req._target not supported for subselects -> see tests
-    return this._update_totals4 (travel)
-  }})
+      await this._update_totals_supplement(booking)
+      return this._update_totals4(travel)
+    }
+  })
 
   /**
    * Update the Travel's TotalPrice when a Booking Supplement is deleted.
@@ -178,6 +183,15 @@ init() {
 
   this.on ('acceptTravel', req => UPDATE (req._target) .with ({TravelStatus_code:'A'}))
   this.on ('rejectTravel', req => UPDATE (req._target) .with ({TravelStatus_code:'X'}))
+  
+ /**
+   * Update the Booking's TotalSupplPrice
+   */
+ this._update_totals_supplement = async function (booking) {
+  const { totals } = await SELECT.one `coalesce (sum (Price),0) as totals` .from (BookingSupplement.drafts) .where
+   `to_Booking_BookingUUID = ${booking}`
+  return  UPDATE (Booking.drafts, booking) .with({TotalSupplPrice: totals})
+}
 
 /**
    * Calculate the progress of the travel booking.
